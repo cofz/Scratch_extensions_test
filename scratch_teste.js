a0a58fa071de77fb99ec8900006dbbb542d3d949
cofz <cassia.fernandez@usp.br>
2016-07-11 16:46:54 +0100

    Update scratch_teste.js

--- scratch_teste.js
+++ scratch_teste.js
@@ -7,6 +7,17 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+    
+    // Functions for block with type 'w' will get a callback function as the 
+    // final argument. This should be called to indicate that the block can
+    // stop waiting.
+    ext.wait_random = function(callback) {
+        wait = Math.random();
+        console.log('Waiting for ' + wait + ' seconds');
+        window.setTimeout(function() {
+            callback();
+        }, wait*1000);
+    };
 
     // Block and block menu descriptions
     var descriptor = {
@@ -15,6 +26,7 @@
             // Block type, block name, function name
             [' ', 'my first block', 'my_first_block'],
             [' ', 'my second block', 'my_second_block'],
+            ['w', 'wait random', 'wait_random']
         ]
     };
 
